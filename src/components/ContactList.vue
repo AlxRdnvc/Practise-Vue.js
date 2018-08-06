08583b13f92c5326547a4c596b8d4bf195ef6623
Aleksandar_Radanovic <a.radanovic1984@gmail.com>
2018-08-06 20:10:36 +0200

    list data from contactlist

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Contact List</h1>
+    <div>
+        <h1>Contact List</h1>
+        <table>
+            <thead>
+                <th>Name</th>
+                <th>Email</th>
+            </thead>
+            <tbody>
+                <tr v-for="(contact, key) in contacts" :key="key">
+                    <td>{{ contact.name }}</td>
+                    <td>{{ contact.email }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'ContactList'
+  name: 'ContactList',
+  data() {
+      return {
+          contacts: [
+              {name: "Srecko", email: "Srecko@gmail.com"},
+              {name: "Dara", email: "Dara@gmail.com"},
+              {name: "Mitar", email: "Mitar@gmail.com"},
+              {name: "Nevena", email: "Nevena@gmail.com"}
+          ]
+      };
+  }
 }
 </script>
